f409c7efbad6ff6e2cded807e5eafe8f96ba5aa6
Felipe Ladislau <felipe.s.ladislau@gmail.com>
2019-12-18 11:11:54 -0300

    Detecta com modelos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,25 @@
+// Pega o elemento do vídeo
 const video = document.getElementById('video');
+
+//Carregando os módulos do FaceAPI
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/assets/js/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/assets/js/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/assets/js/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/assets/js/models')
+  ]).then(iniciaVideo)
 
 function iniciaVideo(){
 
+    // Pega o conteúdo da camera
     navigator.getUserMedia(
         { video: {} },
         stream => video.srcObject = stream,
-        err => console.error(err)
+        err => console.error(err) // erro...
     )
 
 }
 
-iniciaVideo()
+video.addEventListener('play', () => {
+    console.log('Lorem ipsum');
+})
